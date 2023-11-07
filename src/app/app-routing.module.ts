104c452009286ce7e1d32e5bddf6bf43729cb716
Farha Ahmed <farhaa853@gmail.com>
2023-11-08 00:18:24 +0200

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MoviesComponent } from './movies/movies.component';
+import { TvComponent } from './tv/tv.component';
+import { PeopleComponent } from './people/people.component';
+import { AboutComponent } from './about/about.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+
+
+
+  { path: 'movies', component: MoviesComponent },
+  { path: 'tv', component: TvComponent },
+  { path: 'people', component: PeopleComponent },
+
+
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  { path: '**', component: NotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { MoviesComponent } from './movies/movies.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { PeopleComponent } from './people/people.component';
+import { RegisterComponent } from './register/register.component';
+import { TvComponent } from './tv/tv.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    FooterComponent,
+    MoviesComponent,
+    NavbarComponent,
+    NotfoundComponent,
+    PeopleComponent,
+    RegisterComponent,
+    TvComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
