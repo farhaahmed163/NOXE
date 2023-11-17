aeee8a01eafb2d6b11f5493b6d603d3a62f32b7a
Farha Ahmed <farhaa853@gmail.com>
2023-11-17 20:05:03 +0200

    login/register/logout

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { FormGroup, FormControl, Validators, ValidationErrors, FormBuilder } from '@angular/forms'
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  error: string = '';
+  isLoading: boolean = false;
+  // ngDoCheck(): void {
+  //   console.log(this.registerForm.get('rePassword')?.errors)
+  //   if (this.registerForm.controls['password'].value == this.registerForm.controls['rePassword'].value) {
+  //     console.log("🤬🤬🤬🤬🤬")
+  //   }
+  //   console.log(this.isLoading)
+  // }
+  constructor(private _AuthService: AuthService, private _Router: Router, private formbuilder: FormBuilder) {
 
+  }
+  loginForm: FormGroup = new FormGroup({
+
+    email: new FormControl(null, [Validators.email, Validators.required]),
+    password: new FormControl(null, [Validators.required, Validators.pattern(/^[A-Z]/)]),
+
+  });
+
+
+  sunbmitloginForm(loginForm: FormGroup) {
+    this.isLoading = true
+    console.log(loginForm.value);
+    this._AuthService.signIn(loginForm.value).subscribe({
+      next: (response) => {
+        this.isLoading = false
+        if (response.message === 'success') {
+          localStorage.setItem('userToken', response.token);
+          this._AuthService.saveUserData();
+          this._Router.navigate(['/home'])
+          console.log(response)
+        }
+      }
+      , error: (err) => {
+        console.log(err)
+        this.isLoading = false
+        this.error = err.error.errors.msg
+      }
+    })
+  }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  isLogin: boolean = false;
+  constructor(private _AuthService: AuthService) {
 
+  }
+  ngOnInit(): void {
+    this._AuthService.userData.subscribe({
+      next: () => {
+        if (this._AuthService.userData.getValue() != null) {
+          this.isLogin = true
+        }
+        else {
+          this.isLogin = false
+        }
+      }
+    })
+
+  }
+  logOut() {
+    this._AuthService.signOut()
+  }
 }
